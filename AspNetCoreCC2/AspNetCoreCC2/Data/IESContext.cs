efc4d456ffcfb22b76954ca2952831d341ca860b
Christovam Munhoz <cmunhozprog02@hotmail.com>
2018-03-29 23:05:57 -0300

    Primeira

---
+++ AspNetCoreCC2/AspNetCoreCC2/Data/IESContext.cs
@@ -0,0 +1,25 @@
+﻿using AspNetCoreCC2.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreCC2.Data
+{
+    public class IESContext : DbContext
+    {
+        public IESContext(DbContextOptions<IESContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Departamento> Departamentos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Departamento>().ToTable("Departamento");
+        }
+    }
+}
---
+++ AspNetCoreCC2/AspNetCoreCC2/Data/IESDbInitializer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreCC2.Models;
+
+namespace AspNetCoreCC2.Data
+{
+    public static class IESDbInitializer
+    {
+        public static void Initialize(IESContext context)
+        {
+            context.Database.EnsureCreated();
+            if (context.Departamentos.Any())
+            {
+                return;
+            }
+
+            var departamentos = new Departamento[]
+            {
+                new Departamento {Nome="Ciência da Computação"},
+                new Departamento {Nome="Ciência de Alimentos"}
+            };
+
+            foreach(Departamento d in departamentos)
+            {
+                context.Departamentos.Add(d);
+            }
+            context.SaveChanges();
+        }
+    }
+}
